a007374f90f8260525a5dec1a4b58f4292abf4b1
ofkindness <andrei.tretyakov@gmail.com>
2017-12-26 06:12:13 +0300

    init

---
+++ README.md
@@ -0,0 +1,30 @@
+# docker-rathena
+
+[![Docker Build Status](https://img.shields.io/docker/build/kafka/rathena.svg)](https://hub.docker.com/r/kafka/rathena/)
+[![](https://images.microbadger.com/badges/image/kafka/rathena.svg)](https://microbadger.com/images/kafka/rathena)
+[![](https://images.microbadger.com/badges/version/kafka/rathena.svg)](https://microbadger.com/images/kafka/rathena)
+
+## What is rAthena?
+
+[rAthena](https://rathena.org) is an open-source cross-platform MMORPG server
+
+
+## How to use this image
+
+```console
+$ docker run --link rathena-mysql:mysql -p 127.0.0.1:6900:6900 -p 127.0.0.1:6121:6121 -p 127.0.0.1:5121:5121 --name rathena -d kafka/rathena
+```
+
+where rathena-mysql is mysql container with updated rAthena sql database 
+
+## User Feedback
+
+### Issues
+
+If you have any problems with or questions about this image, please contact through a [GitHub issue](https://github.com/ofkindness/docker-openkore/issues).
+
+### Contributing
+
+You are invited to contribute new features, fixes, or updates, large or small; It is always thrilled to receive pull requests, and process them.
+
+Before you start to code, it is recommend discussing your plans through a [GitHub issue](https://github.com/ofkindness/docker-openkore/issues), especially for ambitious contributions.
---
+++ docker-entrypoint.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+#set -e
+
+if [ -z "$MYSQL_PORT_3306_TCP_ADDR" ]; then
+	echo >&2 'error: missing MYSQL_PORT_3306_TCP_ADDR environment variable'
+	echo >&2 ' Did you forget to --link rathena-mysql:mysql ?'
+	exit 1
+fi
+
+sed -i "s|login_server_ip: 127.0.0.1|login_server_ip: ${MYSQL_PORT_3306_TCP_ADDR}|g" ./conf/inter_athena.conf
+sed -i "s|login_server_id: ragnarok|login_server_id: ${MYSQL_ENV_MYSQL_USER}|g" ./conf/inter_athena.conf
+sed -i "s|login_server_pw: ragnarok|login_server_pw: ${MYSQL_ENV_MYSQL_PASSWORD}|g" ./conf/inter_athena.conf
+sed -i "s|login_server_db: ragnarok|login_server_db: ${MYSQL_ENV_MYSQL_DATABASE}|g" ./conf/inter_athena.conf
+
+sed -i "s|map_server_ip: 127.0.0.1|map_server_ip: ${MYSQL_PORT_3306_TCP_ADDR}|g" ./conf/inter_athena.conf
+sed -i "s|map_server_id: ragnarok|map_server_id: ${MYSQL_ENV_MYSQL_USER}|g" ./conf/inter_athena.conf
+sed -i "s|map_server_pw: ragnarok|map_server_pw: ${MYSQL_ENV_MYSQL_PASSWORD}|g" ./conf/inter_athena.conf
+sed -i "s|map_server_db: ragnarok|map_server_db: ${MYSQL_ENV_MYSQL_DATABASE}|g" ./conf/inter_athena.conf
+
+sed -i "s|char_server_ip: 127.0.0.1|char_server_ip: ${MYSQL_PORT_3306_TCP_ADDR}|g" ./conf/inter_athena.conf
+sed -i "s|char_server_id: ragnarok|char_server_id: ${MYSQL_ENV_MYSQL_USER}|g" ./conf/inter_athena.conf
+sed -i "s|char_server_pw: ragnarok|char_server_pw: ${MYSQL_ENV_MYSQL_PASSWORD}|g" ./conf/inter_athena.conf
+sed -i "s|char_server_db: ragnarok|char_server_db: ${MYSQL_ENV_MYSQL_DATABASE}|g" ./conf/inter_athena.conf
+
+sed -i "s|ipban_db_ip: 127.0.0.1|ipban_db_ip: ${MYSQL_PORT_3306_TCP_ADDR}|g" ./conf/inter_athena.conf
+sed -i "s|ipban_db_id: ragnarok|ipban_db_id: ${MYSQL_ENV_MYSQL_USER}|g" ./conf/inter_athena.conf
+sed -i "s|ipban_db_pw: ragnarok|ipban_db_pw: ${MYSQL_ENV_MYSQL_PASSWORD}|g" ./conf/inter_athena.conf
+sed -i "s|ipban_db_db: ragnarok|ipban_db_db: ${MYSQL_ENV_MYSQL_DATABASE}|g" ./conf/inter_athena.conf
+
+sed -i "s|log_db_ip: 127.0.0.1|log_db_ip: ${MYSQL_PORT_3306_TCP_ADDR}|g" ./conf/inter_athena.conf
+sed -i "s|log_db_id: ragnarok|log_db_id: ${MYSQL_ENV_MYSQL_USER}|g" ./conf/inter_athena.conf
+sed -i "s|log_db_pw: ragnarok|log_db_pw: ${MYSQL_ENV_MYSQL_PASSWORD}|g" ./conf/inter_athena.conf
+sed -i "s|log_db_db: ragnarok|log_db_db: ${MYSQL_ENV_MYSQL_DATABASE}|g" ./conf/inter_athena.conf
+
+if [ -z "$PUBLIC_IP" ]; then
+	PUBLIC_IP=$(hostname -i)
+fi
+
+sed -i "s/\/\/bind_ip: 127.0.0.1/bind_ip: ${PUBLIC_IP}/g" ./conf/login_athena.conf
+sed -i "s/\/\/char_ip: 127.0.0.1/char_ip: ${PUBLIC_IP}/g" ./conf/char_athena.conf
+sed -i "s/\/\/map_ip: 127.0.0.1/map_ip: ${PUBLIC_IP}/g" ./conf/map_athena.conf
+
+exec "$@"
